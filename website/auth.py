01e3ea7bdaf000fd59c298377da7c17016e34d3b
Mikeychocho <mikeychocho@gmail.com>
2023-06-10 09:23:24 +0100

    fixed the endpoint issue

--- website/auth.py
+++ website/auth.py
@@ -5,6 +5,8 @@ from . import db, mail
 from flask_login import login_required, login_user, logout_user, current_user
 from flask_login import LoginManager
 from datetime import timedelta
+from flask_mail import Message
+from itsdangerous import URLSafeSerializer as Serializer
 
 
 # Create Flask app instance
@@ -15,6 +17,45 @@ login_manager = LoginManager(app)
 
 # Create auth Blueprint
 auth = Blueprint('auth', __name__)
+
+def send_confirmation_email(user):
+    serializer = Serializer(app.config['SECRET_KEY'])
+    token = serializer.dumps(user)
+    confirm_url = url_for('auth.signup', token=token, _external=True)
+    
+    subject = 'Confirm Your Email Address'
+    body = f'''Please click the following link to confirm your email address: 
+
+    {confirm_url}
+
+    If you didn't request this email, please ignore this message.
+    
+    '''
+    
+    # Create a message instance
+    message = Message(subject=subject, body=body, recipients=[user.email])
+    
+    # Send the email
+    mail.send(message)
+
+@auth.route('/signup/<token>', methods=['GET'])
+def confirm_email(token):
+    try:
+        new_user = Serializer.loads(token, max_age=86400)
+    except:
+        flash('The confirmation link is invalid or has expired.', category='error')
+        return redirect(url_for('auth.signin'))
+    
+    # new_user = User.query.filter_by(email=user.email).first()
+    if new_user:
+        # Adds the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Flashes success message and redirects to home page
+        flash('Account created for ' + new_user.username, category='success')
+        login_user(new_user)
+        return redirect(url_for('views.home'))
 
 @auth.route('/signup', methods=['GET', 'POST'], endpoint='signup')
 def signup():
@@ -54,13 +95,9 @@ def signup():
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
             # Saves user with the retrieved data to a variable
             new_user = User(email=email, name=name, username=username, gender=gender, password=hashed_password)
-            # Adds the new user to the database
-            db.session.add(new_user)
-            db.session.commit()
-
-            # Flashes success message and redirects to home page
-            flash('Account created for ' + new_user.username, category='success')
-            return redirect(url_for('views.home'))
+            
+            # Send the confirmation email
+            send_confirmation_email(new_user)
 
     return render_template('auth.html')
 
@@ -79,13 +116,13 @@ def signin():
         if user:
             # Validates the encrypted password
             if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                flash('Logged in successfully!', category='success')
+                flash(f'Logged in successfully as {user.username}', category='success')
 
                 # Checks if user choose to stay logged in
                 if remember:
                     # Logs in user and stay logged in for 7 days
                     login_manager.remember_cookie_duration = timedelta(days=7)
-                    login_user(user, remember=False)
+                    login_user(user)
                 else:
                     # Logs in user
                     login_user(user, remember=False)
@@ -112,29 +149,29 @@ def logout():
 
 
 def send_mail(user):
-    token = user.get_token()
+    serial = Serializer(app.config['SECRET_KEY'], expires_sec=300)
+    token = serial.dumps({user.id}).decode('utf-8')
     msg = Message('Password Reset Sent', recipients=[user.email], sender='')
     msg.body = f'''
     To reset Password, please follow the link below.
 
-    {url_for()}
+    {url_for('auth.forgetpassword', token=token, _external=True)}
     
     If you didn't send a reset password request, please ignore this message.
     '''
+    mail.send(msg)
 
 
 @auth.route('/forgetpassword', methods=['POST'], endpoint='forgetpassword')
 def forgetpassword():
     # Retrieves data from Javascript fetch api
     email = request.form.get('forget_password_email')
-    print(f'Email: {email}')
 
     # Gets user with the email if they exist and assign a storage value
     user = User.query.filter_by(email=email).first()
     
     # Handles if User associated with the email does exist
     if user:
-        print(f'Email: {email}')
         send_mail(user)
 
         flash('Password reset link has been sent! Check your email and click on it to proceed.', category='success')
@@ -147,10 +184,23 @@ def forgetpassword():
     return render_template('auth.html')
 
 @auth.route('/forgetpassword/<token>', methods=['GET', 'POST'])
-def verify_token():
+def reset_token(token):
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
     user = User.verify_token(token)
     if user is None:
         flash('Invalid token or token has expired, please try again', category='error')
-        return redirect(url_for('views.forgetpassword'))
+        return redirect(url_for('auth.forgetpassword'))
+    
+    if password != password2:
+        flash('Password are not the same!', category='error')
+        return render_template('change_password.html', token=token)
+    elif password == password2:
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Password changed successfully! Login now to continue shopping')
+        return redirect(url_for('auth.signin'))
     
     return render_template('change_password.html')
--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/', methods=['GET', 'POST'], endpoint='home')
 def home():
     # Check if the user is authenticated
     if current_user.is_authenticated:
@@ -17,31 +17,31 @@ def home():
             session['show_flash'] = True
     
     # Render the home.html template
-    return render_template('home.html', user=current_user)
+    return render_template('home.html', user=current_user, endpoint='home')
 
 @views.route('/menu', methods=['GET', 'POST'], endpoint='menu')
 def menu():
     
-    return render_template('menu.html', user=current_user)
+    return render_template('menu.html', user=current_user, endpoint='menu')
 
 @views.route('/cart', methods=['GET', 'POST'], endpoint='cart')
 @login_required
 def cart():
     
-    return render_template('cart.html', user=current_user)
+    return render_template('cart.html', user=current_user, endpoint='cart')
 
 @views.route('/allergy', methods=['GET', 'POST'], endpoint='allergy')
 @login_required
 def allergy():
     
-    return render_template('allergy.html', user=current_user)
+    return render_template('allergy.html', user=current_user, endpoint='allergy')
 
 @views.route('/aboutus', methods=['GET', 'POST'], endpoint='aboutus')
 def aboutus():
     
-    return render_template('aboutus.html', user=current_user)
+    return render_template('aboutus.html', user=current_user, endpoint='aboutus')
 
 @views.route('/privacy', methods=['GET', 'POST'], endpoint='privacy')
 def privacy():
     
-    return render_template('privacy.html', user=current_user)
+    return render_template('privacy.html', user=current_user, endpoint='privacy')

65a6affb1612f22a9a5646038ca95968abbfbda5
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-08-13 23:26:38 +0100

    debugging for the production server

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
+    print(':::::::::::::::::::::::::::: main.py is fine \n:::::::::::::::::::::::::::::::')
     app.run(debug=True)
     # app.run(debug=False, host='0.0.0.0')

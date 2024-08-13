19d2ab80343d4712cab1e04a71054e1278248648
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-08-13 23:56:22 +0100

    debugging for the production server 5

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 import os
-from website import create_app
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-app = create_app()
+from website import create_app
 
+app = create_app()
 if __name__ == "__main__":
     # app.run(debug=True)
     app.run(debug=False, host='0.0.0.0')
